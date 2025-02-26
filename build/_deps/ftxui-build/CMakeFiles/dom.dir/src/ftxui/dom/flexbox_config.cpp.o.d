d627beb7b55599ba2a4a469452b082bf1c7fa7b3
Dave <48265597+lonely-v3n1x@users.noreply.github.com>
2025-02-26 00:40:42 +0000

    Delete build directory

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/dave/codes/projects/contact_bat/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/dave/codes/projects/contact_bat/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
