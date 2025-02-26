248b9d15525a0bcce952de4c02d95c4361f0624c
lonely-v3n1x <karimyussif63@gmail.com>
2025-02-26 00:22:05 +0000

    basic setup

---
+++ build/CMakeFiles/contactBat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/contactBat.dir/link.d"
+  "CMakeFiles/contactBat.dir/contactBat.cpp.o"
+  "CMakeFiles/contactBat.dir/contactBat.cpp.o.d"
+  "contactBat"
+  "contactBat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/contactBat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/contactBat.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for contactBat.
+# This may be replaced when dependencies are built.
